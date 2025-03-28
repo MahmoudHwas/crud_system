a21e595dbaa5314e9979fba27c44a3d76a8118a8
MahmoudHwas <mahmoudhwas62@gmail.com>
2025-03-29 01:08:11 +0200

    ة

--- routes/route.js
+++ routes/route.js
@@ -4,22 +4,16 @@ const User = require("../models/users")
 const multer = require("multer")
 const fs = require("fs")
 
-var storage = multer.diskStorage({
-    destination: function(req, file, cb)  {
-        cb(null, "./uploads");
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "public/uploads"); // حفظ الصور داخل مجلد public
     },
     filename: function (req, file, cb) {
-        cb(null, file.fieldname +"_" + Date.now() +"_" + file.originalname);
-       
-        
+        cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
     }
-
 });
 
-var upload = multer({
-    storage: storage
-
-}).single('image')
+const upload = multer({ storage: storage }).single("image");
 
 // insert user into databse
 router.post('/add', upload ,  (req, res) => {
